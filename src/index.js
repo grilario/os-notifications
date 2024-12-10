7ac8449274a30dc51a8ffd0f3c064f37d27b48cf
Luis Fernando <luisfernandinho094@gmail.com>
2024-12-09 23:30:26 -0300

    updates orders every 10 minutes

--- src/index.js
+++ src/index.js
@@ -4,22 +4,28 @@ import { setPendingOrders } from "./storage.js";
 const last7Days = new Date()
 last7Days.setDate(last7Days.getDate() - 7)
 
-const orders = await getOrders()
+async function handleOrders() {
+  const orders = await getOrders()
 
-const pendingOrders = orders.filter(order => {
-  const isPedding = order.status = "Pendente"
-  const isEmptyTec = order.gse_tec_id == ""
-  const isFromLastDay = new Date(order.data) > last7Days
+  const pendingOrders = orders.filter(order => {
+    const isPedding = order.status = "Pendente"
+    const isEmptyTec = order.gse_tec_id == ""
+    const isFromLastDay = new Date(order.data) > last7Days
 
-  return isPedding && isEmptyTec && isFromLastDay
-})
+    return isPedding && isEmptyTec && isFromLastDay
+  })
 
-for (const order of pendingOrders) {
-  console.log("")
-  console.log("Secretaria:", order.gse_sec_id)
-  console.log("Local:", order.gse_dep_id)
-  console.log("Dia:", order.data.toDateString())
-  console.log("Status:", order.status)
+  for (const order of pendingOrders) {
+    console.log("Secretaria:", order.gse_sec_id)
+    console.log("Local:", order.gse_dep_id)
+    console.log("Dia:", order.data.toDateString())
+    console.log("Status:", order.status)
+  }
+
+  await setPendingOrders(pendingOrders)
 }
 
-await setPendingOrders(pendingOrders)
+await handleOrders()
+
+const tenMinutes = 1_000 * 60 * 10
+setInterval(handleOrders, tenMinutes)
