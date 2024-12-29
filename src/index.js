90c886a0864032a4c8dd38cdd33e0ec45c04d19e
Luis Fernando <luisfernandinho094@gmail.com>
2024-12-29 17:16:14 -0300

    add catch errors

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
 import { getOrders } from "./orders.js";
 import { setPendingOrders } from "./storage.js";
 
-const last7Days = new Date()
-last7Days.setDate(last7Days.getDate() - 7)
+const last7Days = new Date();
+last7Days.setDate(last7Days.getDate() - 7);
 
 async function handleOrders() {
-  const orders = await getOrders()
+  const orders = await getOrders();
 
-  const pendingOrders = orders.filter(order => {
-    const isPedding = order.status = "Pendente"
-    const isEmptyTec = order.gse_tec_id == ""
-    const isFromLastDay = new Date(order.data) > last7Days
+  const pendingOrders = orders.filter((order) => {
+    const isPedding = (order.status = "Pendente");
+    const isEmptyTec = order.gse_tec_id == "";
+    const isFromLastDay = new Date(order.data) > last7Days;
 
-    return isPedding && isEmptyTec && isFromLastDay
-  })
+    return isPedding && isEmptyTec && isFromLastDay;
+  });
 
   for (const order of pendingOrders) {
-    console.log("Secretaria:", order.gse_sec_id)
-    console.log("Local:", order.gse_dep_id)
-    console.log("Dia:", order.data.toDateString())
-    console.log("Status:", order.status)
+    console.log("Secretaria:", order.gse_sec_id);
+    console.log("Local:", order.gse_dep_id);
+    console.log("Dia:", order.data.toDateString());
+    console.log("Status:", order.status);
   }
 
-  await setPendingOrders(pendingOrders)
+  await setPendingOrders(pendingOrders);
 }
 
-await handleOrders()
+const tenMinutes = 1_000 * 60 * 10;
 
-const tenMinutes = 1_000 * 60 * 10
-setInterval(handleOrders, tenMinutes)
+await handleOrders().catch((err) => console.error(err));
+
+setInterval(() => {
+  handleOrders().catch((err) => console.error(err));
+}, tenMinutes);
